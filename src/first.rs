840381009078c0d1591178352c8f978ffe713ba5
Catalin Vajaiala <catalin.vajaiala@gmail.com>
2021-06-18 11:36:01 +0300

    Add three different linked-list implementations

---
+++ src/first.rs
@@ -0,0 +1,94 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> List {
+        return List {
+            head: Link::Empty
+        }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut p : Link = mem::replace(&mut self.head, Link::Empty);
+
+        let mut node = p;
+
+        loop {
+            match node {
+                Link::Empty => break,
+                Link::More(cur_node) => {
+                    p = cur_node.next;
+                    node = p;
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        // Create empty list
+        let mut list = List::new();
+
+        // Check empty list works right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more data
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+
+        drop(list);
+    }
+}
